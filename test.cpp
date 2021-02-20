abe5ede2304062ebaa2f0745ed27554558db9932
angel phang <1201302882@student.mmu.edu.my>
2021-02-20 15:26:06 +0800

    test

--- test.cpp
+++ test.cpp
@@ -1,2 +1,27 @@
 //Hi
 //I am Angel Phang Ting Ting
+//done
+#include <iostream>
+#include <string>
+using namespace std;
+
+int sum (int, int);  // function prototype
+                     // describes the function (RT, Name, parameters)
+int main(){
+    int x = 5, y = 1;
+
+    int total;
+    total = sum(x,y);
+    
+    cout << total << endl;
+    return 0;
+}
+
+int sum (int a, int y)  // value of main(x)--->sum(a)
+                        // main(y)--->sum(y)
+                        // at the end the retrun statement
+                        // returns a value for main(total)
+{
+    return a+y;
+}
+
